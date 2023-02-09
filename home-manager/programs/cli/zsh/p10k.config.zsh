0871fe6317e7ca61c1c4018b0391a625c701204d
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-02-09 18:35:57 +0700

    fix: Move p10k instant prompt

--- home-manager/programs/cli/zsh/p10k.config.zsh
+++ home-manager/programs/cli/zsh/p10k.config.zsh
@@ -1,6 +1,4 @@
+
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
-
-source ~/.config/zimfw/modules/powerlevel10k/powerlevel10k.zsh-theme
-source ~/.config/zsh/p10k.zsh
