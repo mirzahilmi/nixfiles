24c8157321c7e41f49183b82e6b0d0313460fc21
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-02-15 16:56:54 +0700

    feat: Add support for L2TP IPSec VPN Connection

--- hosts/nixsina/default.nix
+++ hosts/nixsina/default.nix
@@ -25,5 +25,10 @@
     networkmanager.enable = true;
   };
 
-  services.openssh.enable = true;
+  services.strongswan = {
+    enable = true;
+    secrets = [
+      "ipsec.d/ipsec.nm-l2tp.secrets"
+    ];
+  };
 }
