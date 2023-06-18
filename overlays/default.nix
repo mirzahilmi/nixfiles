2f0954175f44f0e3f553b93aa28261825b1afc76
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-06-18 13:42:16 +0700

    feat: Enable unstable nixpkgs through overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,13 +1,13 @@
-{...}: {
-  # This one brings our custom packages from the 'pkgs' directory
+{inputs, ...}: {
+  # This one brings custom packages from the 'pkgs' directory
   extras = final: _prev: import ../pkgs {pkgs = final;};
 
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.unstable'
-  # unstable-packages = final: _prev: {
-  #   unstable = import inputs.nixpkgs-unstable {
-  #     system = final.system;
-  #     config.allowUnfree = true;
-  #   };
-  # };
+  # Unstable nixpkgs set (declared in the flake inputs) are
+  # accessible through 'pkgs.unstable'
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
 }
