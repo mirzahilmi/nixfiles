38372c7241436b1035a810fefb2957128c871d33
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-07-24 23:50:09 +0700

    refactor: Move nix optimization to its own module

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -4,23 +4,9 @@
     ./fonts.nix
     ./kernel.nix
     ./locale.nix
+    ./optimization.nix
     ./xdg-spec.nix
   ];
-
-  nixpkgs.config.allowUnfree = true;
-
-  nix = {
-    settings = {
-      experimental-features = "nix-command flakes";
-      auto-optimise-store = true;
-    };
-
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 1w";
-    };
-  };
 
   system.stateVersion = "23.11";
 }
