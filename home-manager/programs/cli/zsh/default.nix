ac75c190a62f9d0cf16a760b570e1d3c10ed20a9
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-11-04 16:29:07 +0700

    feat: Add nix-direnv shell integration snippet

--- home-manager/programs/cli/zsh/default.nix
+++ home-manager/programs/cli/zsh/default.nix
@@ -19,8 +19,31 @@
 
     initExtra = ''
       export WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
-      export DIRENV_LOG_FORMAT=\'\'
-      bindkey '^?' backward-kill-word
+      export DIRENV_LOG_FORMAT=""
+      nixify() {
+        if [ ! -e ./.envrc ]; then
+          echo "use nix" > .envrc
+          direnv allow
+        fi
+        if [[ ! -e shell.nix ]] && [[ ! -e default.nix ]]; then
+          cat > default.nix <<'EOF'
+          with import <nixpkgs> {};
+          mkShell {
+            nativeBuildInputs = [
+              bashInteractive
+            ];
+          }
+          EOF
+        fi
+      }
+      flakify() {
+        if [ ! -e flake.nix ]; then
+          nix flake new -t github:nix-community/nix-direnv .
+        elif [ ! -e .envrc ]; then
+          echo "use flake" > .envrc
+          direnv allow
+        fi
+      }
     '';
 
     history = {
