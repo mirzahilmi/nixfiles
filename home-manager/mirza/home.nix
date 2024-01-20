cee03dac1ed74cb47f675dd0279c46066295b33a
mirza@Archturus <exquisitemirza@student.ub.ac.id>
2024-01-20 17:57:28 +0700

    refactor: Change activated GNOME Extension array style

--- home-manager/mirza/home.nix
+++ home-manager/mirza/home.nix
@@ -1,5 +1,4 @@
 { inputs, outputs, ... }:
-
 {
   home = {
     username = "mirza";
@@ -10,13 +9,27 @@
     enable = true;
     settings = {
       "org/gnome/desktop/interface".color-scheme = "prefer-dark";
-      "org/gnome/shell".enabled-extensions = "['drive-menu@gnome-shell-extensions.gcampax.github.com', 'user-theme@gnome-shell-extensions.gcampax.github.com', 'dash-to-dock@micxgx.gmail.com', 'blur-my-shell@aunetx', 'Vitals@CoreCoding.com', 'clipboard-indicator@tudmotu.com', 'unlockDialogBackground@sun.wxg@gmail.com']";
+      
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = [
+          "drive-menu@gnome-shell-extensions.gcampax.github.com"
+          "user-theme@gnome-shell-extensions.gcampax.github.com"
+          "dash-to-dock@micxgx.gmail.com"
+          "blur-my-shell@aunetx"
+          "Vitals@CoreCoding.com"
+          "clipboard-indicator@tudmotu.com"
+          "unlockDialogBackground@sun.wxg@gmail.com"
+        ];
+      };
+
       "org/gnome/desktop/background" = {
         color-shading-type = "solid";
         picture-options = "zoom";
         picture-uri = "file://" + ../assets/gnome-abstract-dark.png;
+        picture-uri-dark = "file://" + ../assets/gnome-abstract-dark.png;
       };
-    }
+    };
   };
 
   programs.git.enable = true;
