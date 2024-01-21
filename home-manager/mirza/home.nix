36dcd13c6cd22d904d53033feb9f86597e7278a0
mirza@Archturus <exquisitemirza@student.ub.ac.id>
2024-01-21 12:58:03 +0700

    refactor: Move gnome-tweaks to its own module

--- home-manager/mirza/home.nix
+++ home-manager/mirza/home.nix
@@ -1,38 +1,21 @@
-{ inputs, outputs, ... }:
+{ pkgs, inputs, outputs, ... }:
 {
+  imports = [
+    ../programs/gnome-tweaks
+  ];
+
   home = {
     username = "mirza";
     homeDirectory = "/home/mirza";
   };
 
-  dconf = {
-    enable = true;
-    settings = {
-      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
-      
-      "org/gnome/shell" = {
-        disable-user-extensions = false;
-        enabled-extensions = [
-          "drive-menu@gnome-shell-extensions.gcampax.github.com"
-          "user-theme@gnome-shell-extensions.gcampax.github.com"
-          "dash-to-dock@micxgx.gmail.com"
-          "blur-my-shell@aunetx"
-          "Vitals@CoreCoding.com"
-          "clipboard-indicator@tudmotu.com"
-          "unlockDialogBackground@sun.wxg@gmail.com"
-        ];
-      };
+  dconf.enable = true;
 
-      "org/gnome/desktop/background" = {
-        color-shading-type = "solid";
-        picture-options = "zoom";
-        picture-uri = "file://" + ../assets/gnome-abstract-dark.png;
-        picture-uri-dark = "file://" + ../assets/gnome-abstract-dark.png;
-      };
-    };
-  };
-
-  programs.git.enable = true;
+  home.packages = with pkgs; [
+    floorp
+    vesktop
+    cool-retro-term
+  ];
 
   home.stateVersion = "23.11";
 }
