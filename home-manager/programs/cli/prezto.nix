13a2655913e70f48a8df8f6cd8ae2ff63ef1c7ea
mirza@Archturus <exquisitemirza@student.ub.ac.id>
2024-01-22 13:16:27 +0700

    feat: Add zsh pmodules

--- home-manager/programs/cli/prezto.nix
+++ home-manager/programs/cli/prezto.nix
@@ -1,7 +1,16 @@
 {
   programs.zsh.prezto = {
     enable = true;
-    pmodules = programs.zsh.prezto.pmodules ++ [
+    pmodules = [
+      "environment"
+      "terminal"
+      "editor"
+      "history"
+      "directory"
+      "spectrum"
+      "utility"
+      "completion"
+      "prompt"
       "syntax-highlighting"
       "history-substring-search"
       "autosuggestions"
