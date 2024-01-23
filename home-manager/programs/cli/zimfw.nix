a7e95762f14c514b76dc3f9fb41d185868da32d3
mirza@Archturus <exquisitemirza@student.ub.ac.id>
2024-01-23 16:22:31 +0700

    fix: Fix zimfw modules not found

--- home-manager/programs/cli/zimfw.nix
+++ home-manager/programs/cli/zimfw.nix
@@ -1,8 +1,6 @@
 {
   programs.zsh.zimfw = {
     enable     = true;
-    homeDir    = "$XDG_CONFIG_HOME/zimfw/plugins";
-    configFile = "$XDG_CONFIG_HOME/zimfw/zimrc";
     zmodules   = [
       "completion"
       "input"
