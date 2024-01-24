cc76ffbac59ec04a22e16c4bb38ca48406de6f52
mirza@Archturus <exquisitemirza@student.ub.ac.id>
2024-01-24 15:50:21 +0700

    feat: Add GNOME Extensions personal settings with DCONF

--- home-manager/programs/gnome-tweaks/default.nix
+++ home-manager/programs/gnome-tweaks/default.nix
@@ -1,18 +1,12 @@
-{ pkgs, ... }: 
 let
   gnomeSchema = "org/gnome";
   desktopSchema = "${gnomeSchema}/desktop";
   wallpaperPath = "file://" + ../../assets/gnome-abstract-dark.png;
 in {
-  home.packages = with pkgs; [
-    gnome.gnome-tweaks
-    gnomeExtensions.blur-my-shell
-    gnomeExtensions.clipboard-indicator
-    gnomeExtensions.dash-to-dock
-    gnomeExtensions.appindicator
-    gnomeExtensions.vitals
+  imports = [
+    (import ./extensions.nix { inherit gnomeSchema; })
   ];
-  
+
   dconf.settings = {
     "${desktopSchema}/interface" = {
       color-scheme = "prefer-dark";
@@ -22,19 +16,6 @@ in {
     };
     # "${gnomeSchema}/nautilus/desktop".font = "IBM Plex 10";
     
-    "${gnomeSchema}/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = [
-        "drive-menu@gnome-shell-extensions.gcampax.github.com"
-        "user-theme@gnome-shell-extensions.gcampax.github.com"
-        "dash-to-dock@micxgx.gmail.com"
-        "blur-my-shell@aunetx"
-        "Vitals@CoreCoding.com"
-        "clipboard-indicator@tudmotu.com"
-        "unlockDialogBackground@sun.wxg@gmail.com"
-      ];
-    };
-
     "${desktopSchema}/background" = {
       color-shading-type = "solid";
       picture-options = "zoom";
---
+++ home-manager/programs/gnome-tweaks/extensions.nix
@@ -0,0 +1,76 @@
+{ pkgs, gnomeSchema, ... }: 
+let
+  gnomeShellSchema = "${gnomeSchema}/shell";
+  extensionsSchema = "${gnomeShellSchema}/extensions";
+in {
+  home.packages = with pkgs; [
+    gnome.gnome-tweaks
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.clipboard-indicator
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.appindicator
+    gnomeExtensions.vitals
+  ];
+ 
+  dconf.settings = {
+    "${gnomeShellSchema}" = {
+      disable-user-extensions = false;
+      enabled-extensions = [
+        "drive-menu@gnome-shell-extensions.gcampax.github.com"
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
+        "dash-to-dock@micxgx.gmail.com"
+        "blur-my-shell@aunetx"
+        "Vitals@CoreCoding.com"
+        "clipboard-indicator@tudmotu.com"
+        "unlockDialogBackground@sun.wxg@gmail.com"
+      ];
+    };
+
+    "${extensionsSchema}/blur-my-shell" = {
+      color-and-noise = true;
+      hacks-level = 1;
+    };
+    
+    "${extensionsSchema}/clipboard-indicator" = {
+      display-mode = 0;
+    };
+
+    "${extensionsSchema}/dash-to-dock" = {
+      apply-custom-theme             = false;
+      background-color               = "rgb(36,31,49)";
+      background-opacity             = 0.77000000000000002;
+      click-action                   = "minimize";
+      custom-background-color        = false;
+      custom-theme-shrink            = false;
+      dash-max-icon-size             = 48;
+      disable-overview-on-startup    = false;
+      dock-position                  = "BOTTOM";
+      height-fraction                = 1.0;
+      hot-keys                       = false;
+      hotkeys-overlay                = false;
+      multi-monitor                  = true;
+      preferred-monitor              = -2;
+      preferred-monitor-by-connector = "eDP-2";
+      show-trash                     = false;
+      transparency-mode              = "DEFAULT";
+    };
+
+    "${extensionsSchema}/vitals" = {
+      hide-icons           = false;
+      hide-zeros           = false;
+      include-public-ip    = false;
+      include-static-info  = false;
+      memory-measurement   = 1;
+      menu-centered        = false;
+      network-speed-format = 0;
+      position-in-panel    = 1;
+      show-fan             = false;
+      show-network         = true;
+      show-storage         = false;
+      show-temperature     = true;
+      show-voltage         = true;
+      update-time          = 30;
+      use-higher-precision = false;
+    };
+  };
+}
