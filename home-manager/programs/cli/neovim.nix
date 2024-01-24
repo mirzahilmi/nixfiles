a713d49855c5e86b953d90d1719a051848874987
mirza@Archturus <exquisitemirza@student.ub.ac.id>
2024-01-24 13:53:20 +0700

    fix: Fix nvchad

--- home-manager/programs/cli/neovim.nix
+++ home-manager/programs/cli/neovim.nix
@@ -8,7 +8,6 @@
     source = builtins.fetchGit {
       url = "https://github.com/NvChad/NvChad.git";
       rev = "v2.0";
-      sha256 = "sha256-J4SGwo/XkKFXvq+Va1EEBm8YOQwIPPGWH3JqCGpFnxY=";
     };
   };
 }
