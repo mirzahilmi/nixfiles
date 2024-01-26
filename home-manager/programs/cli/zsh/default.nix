84e47c76361c1d900a7febf91aefa0282e4fb982
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-01-26 15:19:55 +0700

    feat: Add zsh-autosuggestions

--- home-manager/programs/cli/zsh/default.nix
+++ home-manager/programs/cli/zsh/default.nix
@@ -18,8 +18,16 @@
       size = 1000;
     };
 
-    plugins = with pkgs; [
-      zsh-autosuggestions
+    plugins = [
+      {
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.7.0";
+          sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+        };
+      }
       {
         name = "completion";
         file = "init.zsh";
