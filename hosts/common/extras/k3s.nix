78b564dba12a2a5b140551d15e9866d6d307f9f2
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-02 21:38:07 +0700

    refactor: Set k3s kubeconfig permission to 606

--- hosts/common/extras/k3s.nix
+++ hosts/common/extras/k3s.nix
@@ -1,8 +1,11 @@
 {pkgs, ...}: {
   # https://nixos.wiki/wiki/K3s
   environment.systemPackages = with pkgs; [k3s];
-  services.k3s.enable = true;
-  services.k3s.role = "server";
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = "--write-kubeconfig-mode 606";
+  };
   networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
   ];
