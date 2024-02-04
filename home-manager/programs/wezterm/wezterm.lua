f46d72bbd2b57f0aa2c00a893f54f181d2b71b15
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-05 00:42:32 +0700

    refactor: Remove wezterm window padding

--- home-manager/programs/wezterm/wezterm.lua
+++ home-manager/programs/wezterm/wezterm.lua
@@ -3,5 +3,11 @@ return {
     font_size = 14.0,
     color_scheme = "Catppuccin Mocha",
     hide_tab_bar_if_only_one_tab = true,
-    window_decorations = "RESIZE"
+    window_decorations = "RESIZE",
+    window_padding = {
+      left = 0,
+      right = 0,
+      top = 0,
+      bottom = 0,
+    },
 }
