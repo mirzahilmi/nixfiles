1c76cebfea6aa788e17e2b808b3b4e8779436d49
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-04 15:06:41 +0700

    feat: Add tmux-yank

--- home-manager/programs/cli/tmux/default.nix
+++ home-manager/programs/cli/tmux/default.nix
@@ -6,10 +6,10 @@
     baseIndex = 1;
     escapeTime = 0;
     prefix = "C-Space";
-    keyMode = "vi";
     extraConfig = builtins.readFile ./tmux.conf;
     plugins = with pkgs.tmuxPlugins; [
       catppuccin
+      yank
     ];
   };
 }
