5f9b40c334c4148566c4144cead203cf8e29904c
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-04 23:12:56 +0700

    feat: Change tmux theme to gruvbox

--- home-manager/programs/cli/tmux/default.nix
+++ home-manager/programs/cli/tmux/default.nix
@@ -9,7 +9,7 @@
     disableConfirmationPrompt = true;
     extraConfig = builtins.readFile ./tmux.conf;
     plugins = with pkgs.tmuxPlugins; [
-      catppuccin
+      gruvbox 
       yank
     ];
   };
