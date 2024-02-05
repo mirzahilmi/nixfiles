26cb83ae83c91e1f97de42bfd0d1ffbf3157a7d8
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-05 21:56:21 +0700

    refactor: Move zshrc config to nix

--- home-manager/programs/cli/zsh/default.nix
+++ home-manager/programs/cli/zsh/default.nix
@@ -6,16 +6,23 @@
     dotDir = ".config/zsh";
 
     initExtraFirst = ''
-      ##############################
       if [[ -r "${config.xdg.cacheHome}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "${config.xdg.cacheHome}/p10k-instant-prompt-''${(%):-%n}.zsh"
       fi
       source ${config.programs.zsh.zimfw.homeDir}/modules/powerlevel10k/powerlevel10k.zsh-theme
       source ~/.config/zsh/p10k.zsh
-      ##############################
     '';
 
-    initExtra = builtins.readFile ./zshrc;
+    localVariables = {
+      WORDCHARS = "*?_-.[]~=&;!#$%^(){}<>";
+      DIRENV_LOG_FORMAT = "";
+      TERM = "xterm-256color";
+    };
+
+    initExtra = ''
+      bindkey '^H' backward-kill-word
+      zstyle ':prezto:module:tmux:auto-start' local 'yes'
+    '';
 
     history = {
       path = "${config.xdg.configHome}/zsh/history";
