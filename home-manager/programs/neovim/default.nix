7359b204339553d5588c7568a50ef5f58988f506
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-06 15:43:18 +0700

    feat: Add neovim telescope pkg to nixos

--- home-manager/programs/neovim/default.nix
+++ home-manager/programs/neovim/default.nix
@@ -1,6 +1,7 @@
-{
+{pkgs, ...}: {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
   };
+  xdg.dataFile."nvim/telescope-fzf-native-nvim".source = pkgs.vimPlugins.telescope-fzf-native-nvim;
 }
