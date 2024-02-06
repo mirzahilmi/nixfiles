ba0ebdc73d6721fe47dee4243c48e3f80fe29478
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-06 15:49:07 +0700

    feat: Add neovim telescope to module plugins

--- home-manager/programs/neovim/default.nix
+++ home-manager/programs/neovim/default.nix
@@ -2,6 +2,7 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+    plugins = [pkgs.vimPlugins.telescope-fzf-native-nvim];
   };
   xdg.dataFile."nvim/telescope-fzf-native-nvim".source = pkgs.vimPlugins.telescope-fzf-native-nvim;
 }
