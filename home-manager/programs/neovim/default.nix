e1942ca4e60c2abc15aa2fa877819735d11dadfd
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-06 16:16:42 +0700

    refactor: Remove telescope

--- home-manager/programs/neovim/default.nix
+++ home-manager/programs/neovim/default.nix
@@ -1,8 +1,6 @@
-{pkgs, ...}: {
+{
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    plugins = [pkgs.vimPlugins.telescope-fzf-native-nvim];
   };
-  xdg.dataFile."nvim/telescope-fzf-native-nvim".source = pkgs.vimPlugins.telescope-fzf-native-nvim;
 }
