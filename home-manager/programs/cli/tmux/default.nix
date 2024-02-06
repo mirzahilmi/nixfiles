39f205e958d4af2605c9b1ce599f54ab1135ffb3
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-02-06 18:28:28 +0700

    refactor: Test change foot term var

--- home-manager/programs/cli/tmux/default.nix
+++ home-manager/programs/cli/tmux/default.nix
@@ -4,7 +4,6 @@
     mouse = true;
     baseIndex = 1;
     escapeTime = 0;
-    terminal = "screen-truecolor";
     prefix = "C-Space";
     disableConfirmationPrompt = true;
     extraConfig = builtins.readFile ./tmux.conf;
