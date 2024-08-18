092913122d15e0e7578e6b3298cf8fc7d924d290
Mirza Hilmi <dev@mrz.my.id>
2024-08-19 02:23:04 +0700

    refactor: move to appropriate /module

--- home/mirza.nix
+++ home/mirza.nix
@@ -6,27 +6,27 @@
   imports = [
     ./common
 
-    ./app/gnome
-    ./app/vscode
+    ../module/home-manager/gnome
+    ../module/home-manager/vscode
 
-    ./app/obs.nix
-    ./app/openssh.nix
-    ./app/spicetify.nix
+    ../module/home-manager/obs.nix
+    ../module/home-manager/openssh.nix
+    ../module/home-manager/spicetify.nix
 
-    ./app/cli/ohmyposh
+    ../module/home-manager/cli/ohmyposh
 
-    ./app/cli/bat.nix
-    ./app/cli/btop.nix
-    ./app/cli/direnv.nix
-    ./app/cli/fastfetch.nix
-    ./app/cli/fzf.nix
-    ./app/cli/gpg.nix
-    ./app/cli/git.nix
-    ./app/cli/jq.nix
-    ./app/cli/lsd.nix
-    ./app/cli/ripgrep.nix
-    ./app/cli/tmux.nix
-    ./app/cli/zsh.nix
+    ../module/home-manager/cli/bat.nix
+    ../module/home-manager/cli/btop.nix
+    ../module/home-manager/cli/direnv.nix
+    ../module/home-manager/cli/fastfetch.nix
+    ../module/home-manager/cli/fzf.nix
+    ../module/home-manager/cli/gpg.nix
+    ../module/home-manager/cli/git.nix
+    ../module/home-manager/cli/jq.nix
+    ../module/home-manager/cli/lsd.nix
+    ../module/home-manager/cli/ripgrep.nix
+    ../module/home-manager/cli/tmux.nix
+    ../module/home-manager/cli/zsh.nix
 
     ../module/home-manager/wezterm.nix
   ];
