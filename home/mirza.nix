6095911521afc297223210dd02abdfd8cc49bd74
Mirza Hilmi <dev@mrz.my.id>
2024-10-06 01:18:52 +0700

    refactor(home-manager): move out nested module to parent dir

--- home/mirza.nix
+++ home/mirza.nix
@@ -3,31 +3,28 @@
     ./shared
 
     ../modules/home-manager/gnome
+    ../modules/home-manager/ohmyposh
 
+    ../modules/home-manager/bat.nix
+    ../modules/home-manager/btop.nix
+    ../modules/home-manager/direnv.nix
+    ../modules/home-manager/fastfetch.nix
     ../modules/home-manager/foot.nix
+    ../modules/home-manager/fzf.nix
+    ../modules/home-manager/git.nix
+    ../modules/home-manager/gpg.nix
+    ../modules/home-manager/jq.nix
+    ../modules/home-manager/lsd.nix
     ../modules/home-manager/mpv.nix
     ../modules/home-manager/obs.nix
+    ../modules/home-manager/ripgrep.nix
     ../modules/home-manager/spicetify.nix
     ../modules/home-manager/ssh.nix
     ../modules/home-manager/tealdeer.nix
-    ../modules/home-manager/zed.nix
-
-    ../modules/home-manager/cli/ohmyposh
-
-    ../modules/home-manager/cli/bat.nix
-    ../modules/home-manager/cli/btop.nix
-    ../modules/home-manager/cli/direnv.nix
-    ../modules/home-manager/cli/fastfetch.nix
-    ../modules/home-manager/cli/fzf.nix
-    ../modules/home-manager/cli/gpg.nix
-    ../modules/home-manager/cli/git.nix
-    ../modules/home-manager/cli/jq.nix
-    ../modules/home-manager/cli/lsd.nix
-    ../modules/home-manager/cli/ripgrep.nix
-    ../modules/home-manager/cli/tmux.nix
-    ../modules/home-manager/cli/zsh.nix
-
+    ../modules/home-manager/tmux.nix
     ../modules/home-manager/wezterm.nix
+    ../modules/home-manager/zed.nix
+    ../modules/home-manager/zsh.nix
   ];
 
   home = {
