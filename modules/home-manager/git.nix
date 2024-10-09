44c3fabf17993240b0ec664279516d1d314ee555
Mirza Hilmi <dev@mrz.my.id>
2024-10-10 06:09:46 +0700

    feat: add delta git diff pager

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -7,20 +7,24 @@
     ignores = ["*.env" "*.env.json" "*.env.yaml"];
     aliases = {
       a = "add -A";
-      ac = "!git a && git commit -m";
-      dif = "!git diff --name-only --relative --diff-filter=d | xargs bat --diff";
+      ac = "!git a && git commit --message";
+      am = "!git a && git commit --ammend";
+      # dif = "!git diff --name-only --relative --diff-filter=d | xargs bat --diff";
       graph = "log --decorate --oneline --graph";
     };
+    delta.enable = true;
     extraConfig = {
       branch.sort = "committerdate";
       column.ui = "auto";
       commit.gpgSign = true;
       commit.verbose = true;
-      pull.rebase = false;
-      push.autoSetupRemote = true;
       gpg.format = "ssh";
       init.defaultBranch = "master";
+      merge.conflictStyle = "zdiff3";
+      pull.rebase = false;
+      push.autoSetupRemote = true;
       push.default = "simple";
+      rerere.enabled = true;
       user.signingkey = "~/.ssh/id_ed25519.pub";
     };
   };
