d4a2d9902f94f8b6d2b93d3f38d22f96aecb0147
Mirza Hilmi <dev@mrz.my.id>
2024-11-19 23:57:02 +0700

    feat(k3s): add run on docker in module option

--- modules/nixos/k3s.nix
+++ modules/nixos/k3s.nix
@@ -7,6 +7,12 @@
 in {
   options.orchestration.k3s = {
     enable = lib.mkEnableOption "k3s";
+    docker = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      example = false;
+      description = "Docker as container runtime";
+    };
     multiNode = lib.mkOption {
       type = lib.types.bool;
       default = false;
@@ -16,8 +22,11 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    services.k3s.enable = true;
-    services.k3s.role = "server";
+    services.k3s = {
+      enable = true;
+      role = "server";
+      extraFlags = lib.mkIf cfg.docker (toString ["--docker"]);
+    };
     networking.firewall.allowedTCPPorts = [6443];
     networking.firewall.allowedUDPPorts = lib.mkIf cfg.multiNode [8472];
   };
