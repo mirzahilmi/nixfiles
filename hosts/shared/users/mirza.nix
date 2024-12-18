72935ce19657592bbab0827b119ec8a10ae0109e
Mirza Hilmi <dev@mrz.my.id>
2024-12-18 09:41:58 +0700

    refactor(user): remove username variable use

--- hosts/shared/users/mirza.nix
+++ hosts/shared/users/mirza.nix
@@ -1,29 +1,22 @@
-{pkgs, ...}: let
-  userName = "mirza";
-in {
-  users = {
-    extraUsers.${userName} = {
-      isNormalUser = true;
-      shell = pkgs.zsh;
-      description = "Mirza's Avatar";
-      initialHashedPassword = "$y$j9T$UCjJGShH7mrXJaboQNwTZ1$1pL9WizElBvl/FagbPKmus902iujrDMxcFJuFOFRCEC";
-      extraGroups = [
-        "wheel"
-        "audio"
-        "docker"
-        "networkmanager"
-        "video"
-        "wireshark"
-      ];
-    };
+{pkgs, ...}: {
+  users.extraUsers.mirza = {
+    description = "Mirza's Avatar";
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "audio"
+      "docker"
+      "networkmanager"
+      "video"
+      "wireshark"
+    ];
   };
-
   boot.postBootCommands = ''
-    if [[ ! -e /var/lib/AccountsService/users/${userName} ]]; then
+    if [[ ! -e /var/lib/AccountsService/users/mirza ]]; then
       echo '[User]
-      Session=
       Icon=${../../../imgs/otter.png}
-      SystemAccount=false' > /var/lib/AccountsService/users/${userName}
+      SystemAccount=false' > /var/lib/AccountsService/users/mirza
     fi
   '';
 }
