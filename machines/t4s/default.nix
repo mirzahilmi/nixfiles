c36315967562b8ce7864774db38d17f409aeb61d
Mirza Hilmi <dev@mrz.my.id>
2025-04-21 14:36:10 +0000

    refactor: disable default windows path append

--- machines/t4s/default.nix
+++ machines/t4s/default.nix
@@ -3,7 +3,19 @@
     ../shared
     ../../users/mirza/nixos.nix
   ];
-  wsl.enable = true;
-  wsl.defaultUser = "mirza";
+
   networking.hostName = "t4s";
+  wsl = {
+    enable = true;
+    defaultUser = "mirza";
+    wslConf = {
+      interop.appendWindowsPath = false;
+      interop.includePath = false;
+    };
+    extraBin = [
+      # I need this for vim yank pass-through WSL
+      {src = "/mnt/c/Windows/System32/clip.exe";}
+      {src = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe";}
+    ];
+  };
 }
