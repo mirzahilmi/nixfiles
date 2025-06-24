5767d5dfcefcdafbcd2462d0a84e13850e311008
mirza@t4s <dev@mrz.my.id>
2025-06-24 02:59:55 +0000

    refactor(tmux): move config to its own file

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -29,19 +29,7 @@ in {
       secureSocket = true;
       sensibleOnTop = false;
       shell = "${pkgs.zsh}/bin/zsh";
-
-      extraConfig = ''
-        set -g default-terminal 'tmux-256color'
-        set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
-        set -as terminal-overrides ',*:Setulc=\E[58::2::::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
-        set-option -g status-position top
-        set -g status-style bg=default
-        set-option -g renumber-windows on
-
-        # split pane in current dir
-        bind '"' split-window -v -c "#{pane_current_path}"
-        bind % split-window -h -c "#{pane_current_path}"
-      '';
+      extraConfig = builtins.readFile ./tmux.conf;
     };
   };
 }
