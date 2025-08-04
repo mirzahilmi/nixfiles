9d971241f5c44a88c12c96074e5b2761db86ebb3
mirza@t4nix <dev@mrz.my.id>
2025-08-04 17:47:33 +0700

    refactor(templates): manage python with uv

--- templates/python/flake.nix
+++ templates/python/flake.nix
@@ -13,43 +13,13 @@
         f {
           pkgs = import nixpkgs {inherit system;};
         });
-
-    version = "3.13";
   in {
-    devShells = forEachSupportedSystem ({pkgs}: let
-      concatMajorMinor = v:
-        pkgs.lib.pipe v [
-          pkgs.lib.versions.splitVersion
-          (pkgs.lib.sublist 0 2)
-          pkgs.lib.concatStrings
-        ];
-
-      python = pkgs."python${concatMajorMinor version}";
-    in {
-      default = pkgs.mkShellNoCC {
-        venvDir = ".venv";
-
-        postShellHook = ''
-          venvVersionWarn() {
-                local venvVersion
-                venvVersion="$("$venvDir/bin/python" -c 'import platform; print(platform.python_version())')"
-
-                [[ "$venvVersion" == "${python.version}" ]] && return
-
-                cat <<EOF
-                Warning: Python version mismatch: [$venvVersion (venv)] != [${python.version}]
-                       Delete '$venvDir' and reload to rebuild for version ${python.version}
-                EOF
-          }
-
-          venvVersionWarn
-        '';
-
-        packages = [
-          python.pkgs.venvShellHook
-          python.pkgs.pip
-          pkgs.basedpyright
-          pkgs.black
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          python3
+          uv
+          basedpyright
         ];
       };
     });
