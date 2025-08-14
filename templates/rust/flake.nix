638ce03cebe8c8fee27427bb0c88bfe0ab2eff1b
mirza@t4nix <dev@mrz.my.id>
2025-08-14 15:04:10 +0700

    chore(rust): bump rust flake lock

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -16,40 +16,26 @@
         f {
           pkgs = import inputs.nixpkgs {
             inherit system;
-            overlays = [
-              inputs.self.overlays.default
-            ];
+            overlays = inputs.self.overlays.default;
           };
         });
   in {
-    overlays.default = final: prev: {
-      rustToolchain = with inputs.fenix.packages.${prev.stdenv.hostPlatform.system};
-        combine (with stable; [
-          clippy
-          rustc
-          cargo
-          rustfmt
-          rust-src
-        ]);
-    };
+    overlays.default = import inputs.rust-overlay;
 
     devShells = forEachSupportedSystem ({pkgs}: {
       default = pkgs.mkShell {
-        packages = with pkgs; [
-          rustToolchain
-          openssl
+        buildInputs = with pkgs; [
           pkg-config
-          cargo-deny
-          cargo-edit
-          cargo-watch
-          rust-analyzer
+          (rust-bin.stable."1.86.0".default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+            ];
+          })
+          cargo-machete
+          openssl
           lldb
         ];
-
-        env = {
-          # Required by rust-analyzer
-          RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
-        };
       };
     });
   };
